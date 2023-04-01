c2be3686c1020b53acabc37948f61c8399eeaa3d
swagoverlord <azcooldude8@gmail.com>
2023-04-01 03:46:18 +0200

    Update app-routing.module.ts

--- ang-ecom/src/app/app-routing.module.ts
+++ ang-ecom/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: '', redirectTo: "home", pathMatch: "full"},
+  { path: 'products', loadChildren: () => import('./pages/products/products.module').then(m => m.ProductsModule) },
+  { path: 'product', loadChildren: () => import('./pages/product/product.module').then(m => m.ProductModule) },
+  { path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) },
+  { path: 'contact', loadChildren: () => import('./pages/contact/contact.module').then(m => m.ContactModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
